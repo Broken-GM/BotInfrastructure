161107c9a06453838b543df0bc429d4bb19f4412
Carson-Key <keyc@oregonstate.edu>
2024-04-27 10:11:15 -0700

    Fix: permission issue part 2

--- generate-ecosystem.sh
+++ generate-ecosystem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 cd ../../../../home/ubuntu/Bot/
-touch ecosystem.config.js
-echo "module.exports = {apps : [{name: 'Broken GM Bot - Main',script: 'app/index.js',max_memory_restart: '256M',env: {DISCORD_TOKEN: '$1',DISCORD_CLIENT_ID: '$2'}}]}" | tee ecosystem.config.js
+sudo touch ecosystem.config.js
+echo "module.exports = {apps : [{name: 'Broken GM Bot - Main',script: 'app/index.js',max_memory_restart: '256M',env: {DISCORD_TOKEN: '$1',DISCORD_CLIENT_ID: '$2'}}]}" | sudo tee ecosystem.config.js
