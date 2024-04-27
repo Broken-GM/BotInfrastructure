13e5708ae0d14f648c07b60fc39f9bdc0d2a91b9
Carson-Key <keyc@oregonstate.edu>
2024-04-27 11:54:49 -0700

    Add: Discord bot code

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-console.log("test")
+import { ShardingManager } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const manager = new ShardingManager('./bot.js', { 
+    token: process.env.DISCORD_TOKEN, 
+    totalShards: 10, 
+    shardList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
+});
+
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+
+manager.spawn();
--- start-app.sh
+++ start-app.sh
@@ -2,3 +2,5 @@
 cd ../../../../home/ubuntu/Bot/app
 sudo npm i
 sudo pm2 start ./index.js
+sudo pm2 save
+pm2 startup ubuntu
