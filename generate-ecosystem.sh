8add62d335819cdc53180794e8a9027b39f40d17
Carson-Key <keyc@oregonstate.edu>
2024-04-27 16:00:33 -0700

    Add: path to ecosystem

--- generate-ecosystem.sh
+++ generate-ecosystem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 cd ../../../../home/ubuntu/Bot/
 sudo touch ecosystem.config.js
-echo "module.exports = {apps : [{name: 'Broken GM',script: 'index.js',max_memory_restart: '256M',env: {DISCORD_TOKEN: '$1',DISCORD_CLIENT_ID: '$2'}}]}" | sudo tee ecosystem.config.js
+echo "module.exports = {apps : [{name: 'Broken GM',script: 'app/index.js',max_memory_restart: '256M',env: {DISCORD_TOKEN: '$1',DISCORD_CLIENT_ID: '$2'}}]}" | sudo tee ecosystem.config.js
