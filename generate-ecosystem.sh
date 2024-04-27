c48601b03fb89ff9dee6c204d9c60cac51bc2d70
Carson-Key <keyc@oregonstate.edu>
2024-04-27 16:41:51 -0700

    WIP

--- generate-ecosystem.sh
+++ generate-ecosystem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
-cd ../../../../home/ubuntu/Bot/
+cd ../../../../home/ubuntu/Bot/app
 sudo touch ecosystem.config.js
-echo "module.exports = {apps : [{name: 'Broken GM',script: 'index.js',max_memory_restart: '256M',env: {DISCORD_TOKEN: '$1',DISCORD_CLIENT_ID: '$2'}}]}" | sudo tee ecosystem.config.js
+echo "const ecosystem = {apps : [{name: 'Broken GM',script: 'index.js',max_memory_restart: '256M',env: {DISCORD_TOKEN: '$1',DISCORD_CLIENT_ID: '$2'}}]}\n export default ecosystem" | sudo tee ecosystem.config.js
