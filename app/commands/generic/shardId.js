d1de3c7760000a83111d44e0d38ec7837cff9805
Carson-Key <keyc@oregonstate.edu>
2024-04-27 18:16:03 -0700

    Add: shardId command

---
+++ app/commands/generic/shardId.js
@@ -0,0 +1,10 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('shard-id')
+		.setDescription('Sends the id of the shard'),
+	async execute(interaction) {
+		await interaction.reply({ content: `This shard has the id ${interaction.guild.shardID}`, ephemeral: true });
+	},
+};
