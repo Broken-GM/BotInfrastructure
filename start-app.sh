aacb6982f514a075decc956319e94935c5fa58e9
Carson-Key <keyc@oregonstate.edu>
2024-04-28 11:41:12 -0700

    Add: pm2 to daemon list

--- start-app.sh
+++ start-app.sh
@@ -3,4 +3,4 @@ cd ../../../../home/ubuntu/Bot/app
 sudo npm i
 sudo pm2 start ecosystem.config.cjs
 sudo pm2 save
-pm2 startup ubuntu
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup ubuntu -u ubuntu --hp /home/ubuntu
