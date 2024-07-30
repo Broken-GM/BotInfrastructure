31f21961dafb5a24e9cf6a2d3dcebf69bb5487ff
Carson-Key <carsonkey99@gmail.com>
2024-07-30 11:43:21 -0700

    Fix: import error

--- app/commands/generic/selectMenu.js
+++ app/commands/generic/selectMenu.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } from 'discord.js';
 
 export default {
 	data: new SlashCommandBuilder()
