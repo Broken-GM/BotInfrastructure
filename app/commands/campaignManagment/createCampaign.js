7c83d3bb6582af9b37aba63ae33442414f1acc92
Carson-Key <carsonkey99@gmail.com>
2024-08-01 20:29:53 -0700

    Add: Dynamo test

--- app/commands/campaignManagment/createCampaign.js
+++ app/commands/campaignManagment/createCampaign.js
@@ -1,10 +1,22 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { GetCommand, DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb'
 
 export default {
 	data: new SlashCommandBuilder()
 		.setName('create-campaign')
 		.setDescription('Creates A campaign and associates it to this discord'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'You tested the bot... look at you', ephemeral: true });
+		const client = new DynamoDBClient({})
+
+		const getUserCommand = new GetCommand({
+			TableName: 'users',
+			Key: {
+			  PK: `discord#${interaction.user.id}`
+			},
+		  })
+		const getUserResponse = await client.send(getUserCommand)
+
+		await interaction.reply({ content: `${JSON.parse(getUserResponse?.Item?.attributes ? getUserResponse?.Item?.attributes : "{}")}`, ephemeral: true });
 	},
 };
