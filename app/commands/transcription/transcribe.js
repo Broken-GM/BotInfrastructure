e48e64d280040db932b1b5288bc3bc1c6d4c7401
Carson-Key <carsonkey99@gmail.com>
2024-08-03 18:00:47 -0700

    Add: session creation to transcribe

--- app/commands/transcription/transcribe.js
+++ app/commands/transcription/transcribe.js
@@ -1,6 +1,7 @@
 import { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } from 'discord.js';
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { GetCommand, DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb'
+import { v4 as uuidv4 } from 'uuid';
 
 export default {
 	data: new SlashCommandBuilder()
@@ -47,7 +48,7 @@ export default {
 			campaigns.push({
 				label: campaignAttribute?.displayName,
 				description: `The Campaign called ${campaignAttribute?.displayName}`,
-				value: campaignAttribute?.id,
+				value: { name: campaignAttribute?.displayName, id: campaignAttribute?.id },
 			})
 		}
 
@@ -65,15 +66,65 @@ export default {
 		});
 
 		const collectorFilter = i => i.user.id === interaction.user.id;
+		let campaignSelected
+		let campaignName
 		try {
 			const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
-			await confirmation.update({ content: `${confirmation?.values?.[0]}`, components: [] });
+			campaignSelected = confirmation?.values?.[0]?.id
+			campaignName = confirmation?.values?.[0]?.name
+			await confirmation.update({ content: `Selected ${confirmation?.values?.[0]?.name}`, components: [] });
 		} catch (e) {
 			console.log(e)
 			await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
 			return
 		}
 
-		console.log("continued")
+		const getSelectedCampaignCommand = new GetCommand({
+			TableName: 'transcripts',
+			Key: {
+				PK: 'campaign',
+			  	SK: campaignSelected
+			},
+		})
+		const getSelectedCampaignResponse = await client.send(getSelectedCampaignCommand)
+		const selectedCampaignAttributes = JSON.parse(getSelectedCampaignResponse?.Item?.attributes ? getSelectedCampaignResponse?.Item?.attributes : "{}")
+
+		const sessionId = uuidv4()
+		const sessionCount = (selectedCampaignAttributes?.sessionCount ? selectedCampaignAttributes?.sessionCount : 0) + 1
+
+		const addSessionInput = {
+			TableName: 'transcripts',
+			Item: {
+				PK: 'session',
+				SK: sessionId,
+				id: sessionId,
+				type: 'session',
+				attributes: JSON.stringify({
+					id: sessionId,
+					sessionNumber: sessionCount,
+					start: Date.now()
+				}),
+			},
+		}
+		const updateCampaignInput = {
+			TableName: 'transcripts',
+			Item: {
+				PK: 'campaign',
+				SK: selectedCampaignAttributes?.id,
+				id: selectedCampaignAttributes?.id,
+				type: 'campaign',
+				attributes: JSON.stringify({
+					...selectedCampaignAttributes,
+					sessions: [...selectedCampaignAttributes?.sessions, sessionId],
+					sessionCount: sessionCount
+				}),
+			},
+		}
+		const addSessionCommand = new PutCommand(addSessionInput)
+		const updateCampaignCommand = new PutCommand(updateCampaignInput)
+		await client.send(addSessionCommand)
+		await client.send(updateCampaignCommand)
+
+		await interaction.reply({content: `Transcribing session ${sessionCount} for ${campaignName}`});
 	},
 };
