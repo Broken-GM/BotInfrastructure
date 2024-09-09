452d70fab8c9d32099604f0911108c2caf34f7ee
Carson-Key <carsonkey99@gmail.com>
2024-09-09 15:09:22 -0700

    Add: git clone to userdata

--- app/deployment/initial-install.sh
+++ app/deployment/initial-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-if [ ! -f ./home/ubuntu/Bot/deployment/checks/ranInitialInstal.txt ]; then
+if [ ! -f ./home/ubuntu/Bot/deployment/checks/ranInitialInstall.txt ]; then
     sudo -u ubuntu sh -c 'sudo apt update -y'
     sudo -u ubuntu sh -c 'sudo apt upgrade -y'
     sudo -u ubuntu sh -c 'sudo apt install -y curl'
@@ -7,8 +7,6 @@ if [ ! -f ./home/ubuntu/Bot/deployment/checks/ranInitialInstal.txt ]; then
     sudo -u ubuntu sh -c 'curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -'
     sudo -u ubuntu sh -c 'sudo apt install -y nodejs'
     sudo -u ubuntu sh -c 'sudo npm install pm2 -g'
-    sudo -u ubuntu sh -c 'sudo git clone https://github.com/Broken-GM/Bot.git ./home/ubuntu/Bot'
-    sudo -u ubuntu sh -c 'git config --global --add safe.directory ./home/ubuntu/Bot'
     sudo -u ubuntu sh -c 'sudo chmod +x ./home/ubuntu/Bot/deployment/deploy-development.sh'
     sudo -u ubuntu sh -c 'sudo chmod +x ./home/ubuntu/Bot/deployment/generate-ecosystem.sh'
     sudo -u ubuntu sh -c 'sudo chmod +x ./home/ubuntu/Bot/deployment/start-app.sh'
