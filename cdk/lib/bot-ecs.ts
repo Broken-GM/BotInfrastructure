641f7839db27260e77571606282b2b87fcf9ce39
Carson-Key <carsonkey99@gmail.com>
2024-10-12 16:24:17 -0700

    Add: ECS creation

--- cdk/lib/bot-ecs.ts
+++ cdk/lib/bot-ecs.ts
@@ -1,24 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
-import * as iam from 'aws-cdk-lib/aws-iam';
+
+interface EcsTaskStackProps extends cdk.StackProps {
+    ecrRepoName: string;
+    vpc: ec2.IVpc;
+    subnet: ec2.ISubnet;
+}
 
 export class EcsStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props: EcsTaskStackProps) {
         super(scope, id, props);
         
-        // // ECS IAM Role
-        // const taskRole = new iam.Role(this, 'main-broken-gm-bot-ecs-role', {
-        //     roleName: 'main-broken-gm-bot-ecs',
-        //     assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
-        // });
+        // Grab ECR Repo
+        const repository = ecr.Repository.fromRepositoryName(
+            this,
+            'main-broken-gm-bot-ecs-ecr-repo',
+            props.ecrRepoName
+        );
 
-        // Create an ECS cluster within the VPC
-        // const cluster = new ecs.Cluster(this, 'main-broken-gm-bot-ecs-cluster', {
-        //     vpc,
-        //     clusterName: 'main-broken-gm-bot'
-        // });
+        // Create ECS Cluster
+        const cluster = new ecs.Cluster(this, 'main-broken-gm-bot-ecs-cluster', {
+            vpc: props?.vpc
+        });
+        cluster.addCapacity('main-broken-gm-bot-ecs-scaling-group', {
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+            desiredCapacity: 1,
+            vpcSubnets: { subnets: [props.subnet] }
+        });
+
+        // Create ECS Task
+        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'main-broken-gm-bot-ecs-task');
+        const container = taskDefinition.addContainer('main-broken-gm-bot-ecs-task-container', {
+            image: ecs.ContainerImage.fromEcrRepository(repository),
+            memoryLimitMiB: 512,
+            cpu: 256,
+            logging: new ecs.AwsLogDriver({ streamPrefix: 'main-broken-gm-bot-ecs-task-container' })
+        });
+
+        new ecs.Ec2Service(this, 'main-broken-gm-bot-ecs-service', {
+            cluster: cluster,
+            taskDefinition: taskDefinition,
+        });
     }
 }
