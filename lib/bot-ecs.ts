2d04e9e403d5c9b69d9988dd96f738fc602fd193
Carson-Key <carsonkey99@gmail.com>
2024-10-13 14:07:16 -0700

    Add: IAM Role to ECS Cluster Task

--- lib/bot-ecs.ts
+++ lib/bot-ecs.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { aws_iam as IAM } from 'aws-cdk-lib';
 
 interface EcsTaskStackProps extends cdk.StackProps {
     ecrRepoName: string;
@@ -28,8 +30,27 @@ export class EcsStack extends cdk.Stack {
             desiredCapacity: 1,
         });
 
+        // Create IAM Role
+        const taskRole = new iam.Role(this, 'main-broken-gm-bot-ecs-task-iam-role', {
+            roleName: 'Broken-Gm-Main-Bot',
+            assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
+        });
+    
+        taskRole.addToPolicy(new IAM.PolicyStatement({
+            effect: IAM.Effect.ALLOW,
+            actions: [
+                'dynamodb:*',
+                'kms:decrypt',
+                'kms:encrypt',
+                'secretsmanager:GetSecretValue'
+            ],
+            resources: [
+              '*'
+            ]
+        }));
+
         // Create ECS Task
-        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'main-broken-gm-bot-ecs-task');
+        const taskDefinition = new ecs.Ec2TaskDefinition(this, 'main-broken-gm-bot-ecs-task', { taskRole });
         const container = taskDefinition.addContainer('main-broken-gm-bot-ecs-task-container', {
             image: ecs.ContainerImage.fromEcrRepository(repository),
             memoryLimitMiB: 512,
